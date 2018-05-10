a7e89924cdabe900f8cc59d79930e3214e0419a8
league <league@imac-6.attlocal.net>
2018-05-09 19:58:39 -0700

    change calculator, sleepy head

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -1,7 +1,6 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
 import javax.swing.JOptionPane;
 
 /*
@@ -14,17 +13,21 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+		String nickels = JOptionPane.showInputDialog("How many nickels do you have?");
 		// Convert their answer to an int using Integer.parseInt()
-
+		int ans = Integer.parseInt(nickels);
 		// Ask the user how many dimes they have, and convert their answer
-
+		String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
+		int answer = Integer.parseInt(dimes);
 		// Ask the user how many quarters they have, and convert their answer
-
-		// Calculate how much money the user has and save it in a double variable 
-
+		String quarters = JOptionPane.showInputDialog("How many quarters do you have?");
+		int input = Integer.parseInt(quarters);
+		// Calculate how much money the user has and save it in a double variable
+		double answ = 0.05*ans;
+		double solution = 0.1*answer;
+		double money = 0.25*input;
 		// Tell the user how much money they have
-
+		double total = answ + solution + money;
+				JOptionPane.showMessageDialog(null, "You have " + total);
 	}
 }
-
--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -1,29 +1,40 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
 import javax.swing.JOptionPane;
 
-
 public class SleepyHead {
 
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        
-        boolean isWeekday, isVacation;
-        
-        /* You MUST use the above boolean variables in your code */
-        
-        /*
-         * Ask the user for these values using a confirm dialog like the one below
-         * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
-         */
-    
+		boolean isWeekday, isVacation;
 
+		/* You MUST use the above boolean variables in your code */
 
-        /*
-         * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
-         * print “get up lazybones!” If it is a weekday, and we are on vacation,
-         * print “sleep in”.
-         */
-    }
+		/*
+		 * Ask the user for these values using a confirm dialog like the one below
+		 * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head",
+		 * JOptionPane.YES_NO_OPTION);
+		 */
+		int result = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+		System.out.println(result);
+
+		/*
+		 * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday, print
+		 * “get up lazybones!” If it is a weekday, and we are on vacation, print “sleep
+		 * in”.
+		 */if (result == 0) {
+			result = JOptionPane.showConfirmDialog(null, "Are you on vacation?", "Sleepy Head",
+					JOptionPane.YES_NO_OPTION);
+
+			if (result == 1) {
+				JOptionPane.showMessageDialog(null, "Get up lazybones!");
+			}else {
+				JOptionPane.showMessageDialog(null, "Sleep in");
+			}
+		} else {
+			JOptionPane.showMessageDialog(null, "Sleep in");
+		}
+	}
 }
