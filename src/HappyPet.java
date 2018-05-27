e0a594245fee576afd7fe9f01e71899af529cf62
league <league@wts22.attlocal.net>
2018-05-26 17:25:18 -0700

    TestScores HappyPet

--- src/HappyPet.java
+++ src/HappyPet.java
@@ -10,36 +10,41 @@ public class HappyPet {
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in
 		// a variable
-		String pet = JOptionPane.showInputDialog(null, "What kind of pet do you want to buy.");
-		// 7. REPEAT steps 3 - 6 enough times to make your pet sad!
+		String pet = JOptionPane.showInputDialog(null, "What kind of pet do you want to buy?");
+		// 7. REPEAT steps 3 - 6 enough times to make your pet hate you!
+		for (int i = 0; i < 6; i++) {
+			if (happinessLevel <= -90)
+				break;
 
-		// 3. Use showOptionDialog to ask the user what they want to do to make their
-		// pet happy
-		// (eg: cuddle, food, water, take a walk, groom, clean up poop).
-		// Make sure to customize the title and question too.
-		int task = JOptionPane.showOptionDialog(null, "What do you want to do want to do to make your pet hate you.",
-				"Death Simulation", 0, JOptionPane.INFORMATION_MESSAGE, null,
-				new String[] { "Get it sick by touching him with your filthy hands.",
-						"Make sure it dies a painful death.", "Burn it in your fireplace" },
-				null);
+			// 3. Use showOptionDialog to ask the user what they want to do to make their
+			// pet hate them.
+			// (eg: burn them, kill them, or get them sick.).
+			// Make sure to customize the title and question too.
+			int task = JOptionPane.showOptionDialog(null,
+					"What do you want to do want to do to make your pet hate you?", "Death Simulation", 0,
+					JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Get it sick by touching him with your filthy hands.",
+							"Make sure it dies a painful death.", "Burn it in your fireplace" },
+					null);
 
-		// 5. Use user input to call the appropriate method created in step 4.
-		System.out.println(task);
-		if (task == 1) {
-			painfulDeath();
-		}
-		if (task == 2) {
-			burnToDeath();
-		}
-		if (task == 0) {
-			filthySick();
-		}
+			// 5. Use user input to call the appropriate method created in step 4.
+			System.out.println(task);
+			if (task == 1) {
+				painfulDeath();
+			}
+			if (task == 2) {
+				burnToDeath();
+			}
+			if (task == 0) {
+				filthySick();
+			}
 
-		// 6. If you determine the happiness level is large enough, tell the
-		// user that he loves his pet and use break; to exit for loop.
-if(happinessLevel <= -90) {
-	JOptionPane.showMessageDialog(null, "You must hate your pet!!!");
-}
+			// 6. If you determine the happiness level is large enough, tell the
+			// user that he loves his pet and use break; to exit for loop.
+			if (happinessLevel <= -90) {
+				JOptionPane.showMessageDialog(null, "You must hate your pet!!!");
+			}
+		}
 	}
 
 	// 4. Create methods to handle each of your user selections.
@@ -53,12 +58,12 @@ if(happinessLevel <= -90) {
 
 	static void painfulDeath() {
 		JOptionPane.showMessageDialog(null, " ");
-		happinessLevel = -20;
+		happinessLevel -= 20;
 	}
 
 	static void filthySick() {
 		JOptionPane.showMessageDialog(null, "ACHOO! ROAR!!!!!!   ");
-		happinessLevel = -10;
+		happinessLevel -= 10;
 
 	}
 
---
+++ src/TestScores.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class TestScores {
+
+	public static void main(String[] args) {
+
+		String score = JOptionPane.showInputDialog("What was your test score?");
+		Double.parseDouble(score);
+		if (score >= 7.5) {
+		}
+	}
+}
